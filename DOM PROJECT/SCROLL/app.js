487489bf49ceef3810868e35c6e5451fdef5a7da
moise <mose.ns@hotmail.com>
2020-07-22 21:52:17 +0200

    Scroll set date close link fixed navbar

---
+++ DOM PROJECT/SCROLL/app.js
@@ -0,0 +1,51 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+const date = document.querySelector('.date');
+date.innerHTML = new Date().getFullYear();
+
+
+// ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`;
+    }else{
+        linksContainer.style.height = 0;
+    }
+});
+
+
+// ********** fixed navbar ***********
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener('scroll', function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav');
+    }else{
+        navbar.classList.remove('fixed-nav');
+    }
+
+    //Showing and hiding the back to top link
+    if(scrollHeight > 676){
+        topLink.classList.add('show-link');
+    }else{
+        topLink.classList.remove('show-link');
+    }
+})
+
+
+
+// ********** smooth scroll ************
+// select links
