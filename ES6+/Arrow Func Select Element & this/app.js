6ee67249324dc67e4fc40ebe4a75145f1a0fa90a
moise <mose.ns@hotmail.com>
2020-07-27 14:07:48 +0200

    select element & this with arrow func

---
+++ ES6+/Arrow Func Select Element & this/app.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(){
+    console.log(this);
+    setTimeout(() => {
+        this.style.color = '#000'
+    }, 2000);
+});
+//If we use a fat arr function on the same event, it'll throw an error
+//because this in a fat arr func refers to the window object. In this case, better to use regular function
+//but if we add in the event a method like setTimeout, reg func will throw an err, fat arr func can solve the problem
+
+
+
