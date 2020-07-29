edc057adf6b30a9a4daa13337739f3f0a2de7b32
moise <mose.ns@hotmail.com>
2020-07-29 16:22:28 +0200

    Asynchronous

---
+++ ASYNC FUNC/Asynchronous/app.js
@@ -0,0 +1,16 @@
+//Asynchronous
+//Borwser!!! fetch data, Get geolocalisation setTimeout, setTimer, etc
+
+boilWater(1000);
+console.log('chop carrots');
+for(let i=0; i<10000; i++){
+    console.log('still busy');
+}
+
+function boilWater(time){
+    console.log('boiling water..');
+    setTimeout(() =>{
+        console.log('done.');
+    },time)
+    
+}
