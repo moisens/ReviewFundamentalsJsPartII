0f1be1cb9d41c268462ab7a0dc4d14332b18cf9a
moise <mose.ns@hotmail.com>
2020-07-30 15:12:02 +0200

    AJAX call forom the DOM

--- AJAX/Simple Text/app.js
+++ AJAX/Simple Text/app.js
@@ -1,17 +1,28 @@
-const xhr = new XMLHttpRequest();
-console.log(xhr);
-xhr.open('GET', './api/simple.txt');
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4 && xhr.status === 200){
-        const text = document.createElement('p');
-        text.textContent = xhr.responseText;
-        document.body.appendChild(text)
-    }else{
-        console.log({
-            status: xhr.status,
-            text: xhr.statusText
-        });
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', ()=>{
+    getData();
+});
+
+function getData(){
+    const xhr = new XMLHttpRequest();
+    console.log(xhr);
+    xhr.open('GET', './api/simple.txt');
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            const text = document.createElement('p');
+            text.textContent = xhr.responseText;
+            document.body.appendChild(text)
+        }else{
+            console.log({
+                status: xhr.status,
+                text: xhr.statusText
+            });
+        }
     }
+
+    xhr.send();
 }
 
-xhr.send();
+
+
