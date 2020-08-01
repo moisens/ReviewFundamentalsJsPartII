eea865bb4101132037a60b4d2eebaacdf79491a0
moise <mose.ns@hotmail.com>
2020-08-01 11:56:05 +0200

    loading

--- BASIC PROJECTS/Cocktails/utils/DisplayDrink.js
+++ BASIC PROJECTS/Cocktails/utils/DisplayDrink.js
@@ -1,10 +1,13 @@
 import getElement from './getElement.js';
+import {hideLoading} from './toggleLoading.js';
+
 
 const displayDrinks = ({drinks}) => {
     const section = getElement('.section-center');
     const title = getElement('.title');
     if(!drinks){
         //hide loading
+        hideLoading();
         title.textContent = 'Sorry, no drinks matched your search!';
         section.innerHTML = null;
         return;
@@ -19,6 +22,7 @@ const displayDrinks = ({drinks}) => {
                 </a>`;
     }).join('');
     //hide loading
+    hideLoading();
     title.textContent = '';
     section.innerHTML = newDrinks;
     return section;
--- BASIC PROJECTS/Cocktails/utils/fetchDrink.js
+++ BASIC PROJECTS/Cocktails/utils/fetchDrink.js
@@ -1,5 +1,8 @@
+import {showLoading} from './toggleLoading.js'
+
 
 const fetchDrinks = async (url) => {
+    showLoading();
     try{
         const response = await fetch(url);//GET IS Set by default with fetch
         const data = await response.json();
