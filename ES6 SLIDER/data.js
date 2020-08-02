5c82d2df0428963c82e7e264f1952bdf5756eca2
moise <mose.ns@hotmail.com>
2020-08-02 07:45:34 +0200

    datajs modif

--- ES6 SLIDER/data.js
+++ ES6 SLIDER/data.js
@@ -1,9 +1,9 @@
 const people = [
     {
       img:
-        "images/emma.jpg",
-      name: "emma jacobi",
-      job: "english teacher",
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
+      name: "peter doe",
+      job: "product designer",
       text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
     },
     {
